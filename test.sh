4ba68a7357e2aa93249d0b3b509ef777ac0c4501
Bela Stoyan <bela.stoyan@gmail.com>
2023-07-25 11:48:06 +0200

    better logs

--- test.sh
+++ test.sh
@@ -5,9 +5,13 @@ echo
 echo "Root Config"
 cat /opt/conda/.condarc -n
 # Uses wrong channel alias
+echo "micromamba info"
 micromamba info
+echo "config get channel_alias"
 micromamba config get channel_alias
+echo "switching to test_env_not_base"
 micromamba env create -n test_env_not_base
 micromamba activate test_env_not_base
+echo "micromamba info"
 # Uses correct channel alias
 micromamba info
